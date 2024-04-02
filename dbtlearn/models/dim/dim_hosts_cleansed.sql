68022dd9145b9024fd79834d4ed18176f25d473c
Madhusudhan <sudhan.18187@gmail.com>
2024-04-02 17:45:07 +0530

    add dim_listings_w_hosts.sql & fct_reviews.sql models

---
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,13 @@
+with src_hosts as(
+    select * from {{ ref('src_hosts') }}
+)
+SELECT
+ host_id,
+ NVL(
+ host_name,
+ 'Anonymous'
+ ) AS host_name,
+ is_superhost,
+ created_at,
+ updated_at
+ from src_hosts
